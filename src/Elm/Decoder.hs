45e271079c8f04ceb7084cdb756d394ec453f96e
Casey Webb <notcaseywebb@gmail.com>
2024-06-26 12:39:03 -0500

    Rid shadowed variable warning

--- src/Elm/Decoder.hs
+++ src/Elm/Decoder.hs
@@ -160,13 +160,13 @@ instance HasDecoder ElmValue where
     return $ dx <$$> dy
   render (ElmField name value) = do
     fieldModifier <- asks fieldLabelModifier
-    optionalListFields <- asks optionalListFields
+    optionalListFields' <- asks optionalListFields
     dv <- render value
     let isList = case value of
           (ElmPrimitiveRef (EList value'))
             | value' /= (ElmPrimitive EChar) -> True
           _ -> False
-    if isList && optionalListFields
+    if isList && optionalListFields'
       then return $ "|> optional" <+> dquotes (stext (fieldModifier name)) <+> dv <+> "[]"
       else return $ "|> required" <+> dquotes (stext (fieldModifier name)) <+> dv
   render ElmEmpty = pure (stext "")
